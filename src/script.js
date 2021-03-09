3e51b7180624d4b8310315ef4c4f7e2161494f69
pengallow <43455176+pengallow@users.noreply.github.com>
2021-03-09 16:50:18 -0500

    API now updates day and time of last update
    Day was changed to "Last Updated." Small CSS update to capitalize on the first letter of the list elements.

--- src/script.js
+++ src/script.js
@@ -1,3 +1,23 @@
+function formatDate(timestamp) {
+    // this will calculate the date based on the dt from the api
+    
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day = days[date.getDay()];
+
+    return `${day} ${hours}:${minutes}`;
+}
+// the time is based on the last update in the API, not from when the user accesses
+
 function displayTemperature(response) {
     //console.log(response.data);
     //console.log(response.data.main.temp);
@@ -7,12 +27,14 @@ function displayTemperature(response) {
     let weatherCondition = document.querySelector("#current-condition");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
 
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     weatherCondition.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "3b69d9e884899e81040ee4e357f33f8b";
